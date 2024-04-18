d7091a1bd11be8f158732afc4c8adf7a7f2922cc
Roman Unt <untroman@gmail.com>
2024-04-18 18:29:25 +0200

    Initial

---
+++ app/_layout.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View} from "react-native";
+
+import React from "react";
+
+const RootLayout = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+}
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
