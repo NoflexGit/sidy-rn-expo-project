a0530bdcbcb07e8b1ab1667c2375994ca2990698
Roman Unt <untroman@gmail.com>
2024-04-29 19:12:37 +0200

    Implemented searching

--- app/search/[query].jsx
+++ app/search/[query].jsx
@@ -1,10 +1,53 @@
-import { View, Text } from 'react-native';
+import { View, FlatList } from 'react-native';
+import { useLocalSearchParams } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import useAppwrite from '../../lib/useAppwrite';
+import { searchVideos } from '../../lib/appwrite';
+import VideoCard from '../../components/VideoCard';
+import PageTitle from '../../components/PageTitle';
+import SearchField from '../../components/SearchField';
+import EmptyState from '../../components/EmptyState';
+import { useEffect } from 'react';
 
-const Query = ({ query }) => {
+const Query = () => {
+  const { query } = useLocalSearchParams();
+  const { data: videos } = useAppwrite(() => searchVideos(query));
+
+  useEffect(() => {
+    return () => {};
+  }, [query]);
+
   return (
-    <View>
-      <Text>Search results for "{query}"</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={videos}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <VideoCard
+            title={item.title}
+            creator={item.creator?.username}
+            avatar={item.creator?.avatar}
+            thumbnail={item.thumbnail}
+            video={item.video}
+          />
+        )}
+        ListHeaderComponent={
+          <View className="flex my-6 px-4 space-y-6">
+            <PageTitle title={query} description="Search results" />
+            <SearchField
+              placeholder="Search for something"
+              initialQuery={query}
+            />
+          </View>
+        }
+        ListEmptyComponent={
+          <EmptyState
+            title="No videos found"
+            description={`No videos found for "${query}"`}
+          />
+        }
+      />
+    </SafeAreaView>
   );
 };
 
--- components/SearchField.jsx
+++ components/SearchField.jsx
@@ -1,13 +1,24 @@
-import { View, Text, TextInput } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+  Image,
+} from 'react-native';
 import { twMerge } from 'tailwind-merge';
+import { usePathname, router } from 'expo-router';
+import { useState } from 'react';
+import { icons } from '../constants';
 
-const FormField = ({
-  textClassName,
-  onChangeText,
-  value,
-  placeholder,
-  label,
-}) => {
+const SearchField = ({ textClassName, placeholder, label, initialQuery }) => {
+  const pathname = usePathname();
+  const [query, setQuery] = useState(initialQuery || '');
+
+  const handleQueryChange = (text) => {
+    setQuery(text);
+  };
+
   const wrapperClasses = twMerge(['space-y-2', textClassName]);
 
   return (
@@ -15,17 +26,35 @@ const FormField = ({
       {label && (
         <Text className="text=base font-pmedium text-gray-100">{label}</Text>
       )}
-      <View className="px-4 h-16 w-full items-center bg-black-100 border-2 border-black-200 focus:border-secondary rounded-2xl">
+      <View className="px-4 h-16 w-full items-center flex-row bg-black-100 border-2 border-black-200 focus:border-secondary rounded-2xl">
         <TextInput
           className="w-full flex-1 text-base font-pmedium text-gray-100"
-          value={value}
-          onChangeText={onChangeText}
+          value={query}
+          onChangeText={handleQueryChange}
           placeholder={placeholder}
           placeholderTextColor="#7b7b8b"
         />
+        <TouchableOpacity
+          onPress={() => {
+            if (query === '')
+              return Alert.alert(
+                'Missing Query',
+                'Please input something to search results across database',
+              );
+
+            if (pathname.startsWith('/search')) router.setParams({ query });
+            else router.push(`/search/${query}`);
+          }}
+        >
+          <Image
+            source={icons.search}
+            className="w-5 h-5"
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
 };
 
-export default FormField;
+export default SearchField;
